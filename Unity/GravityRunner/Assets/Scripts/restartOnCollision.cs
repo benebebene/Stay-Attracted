211bbf283fb682429629a5a280e8cb50fe22134b
Lasse Niederkrome <lasseFniederkrome@gmail.com>
2022-11-23 13:51:49 +0100

    Spikes added.
    Added spikes, which resets the scene on collision with the player. You can find a complete Spike with Script attached in the PREFAB-Folder, just drag and drop it to your scene and it should work fine.

---
+++ Unity/GravityRunner/Assets/Scripts/restartOnCollision.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartOnCollision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D coll){
+        if (coll.gameObject.tag =="Player"){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else if (coll.gameObject.tag != "Player"){
+            print ("hi");
+        }
+    }
+}
