06e6ef2e6cf96e8e0ca676a256f079c6be06d04a
Lasse Niederkrome <lasseFniederkrome@gmail.com>
2022-12-03 12:15:37 +0100

    implemented a enemy sprite

--- Unity/GravityRunner/Assets/Scripts/restartOnCollision.cs
+++ Unity/GravityRunner/Assets/Scripts/restartOnCollision.cs
@@ -5,12 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class restartOnCollision : MonoBehaviour
 {
+    [SerializeField]
+    public string test;
     private void OnTriggerEnter2D(Collider2D coll){
         if (coll.gameObject.tag =="Player"){
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
         else if (coll.gameObject.tag != "Player"){
             Destroy(coll.gameObject);
+        
         }
     }
 }
