ed5d899c9b65afa7e168e28725c955de2434aeed
Matthias Wallner <m.wallner@student.uni-tuebingen.de>
2022-12-27 22:38:03 +0100

    making collision detection more efficient
    Replacing OnTriggerEnter2D with OnColissionEnter2D allows for removing many unnecessary trigger-colliders

--- Unity/GravityRunner/Assets/Scripts/PlayerCollision.cs
+++ Unity/GravityRunner/Assets/Scripts/PlayerCollision.cs
@@ -7,7 +7,7 @@ public class PlayerCollision : MonoBehaviour
 {
     public GameObject gameOverScreen;
 
-    private void OnTriggerEnter2D(Collider2D coll){
+    private void OnCollisionEnter2D(Collision2D coll){
         if ((coll.gameObject.tag =="Spike") || (coll.gameObject.tag == "Blob")){
             Time.timeScale = 0f;
             gameOverScreen.SetActive(true);
