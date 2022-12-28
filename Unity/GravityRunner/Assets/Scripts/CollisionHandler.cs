7aecfb693b142155abe5e817db660e1781920e2d
Matthias Wallner <m.wallner@student.uni-tuebingen.de>
2022-12-28 18:29:05 +0100

    Create CollisionHandler.cs

---
+++ Unity/GravityRunner/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler
+{    
+    private CollisionHandler() { }
+
+    
+
+    // assuming that manageCollision() is called by player
+    public static void manageCollision(GameObject self, GameObject other)
+    {
+        if (self.CompareTag("Player") && (other.CompareTag("Spike") || other.CompareTag("Blob")))
+        {
+            LevelHandler.gameOver();
+        }
+    }
+
+}
