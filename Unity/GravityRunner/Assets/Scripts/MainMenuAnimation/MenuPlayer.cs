35f646259f9531b0a0a89927efbc45e68cb85cb9
Julez0910 <116381068+Julez0910@users.noreply.github.com>
2023-01-03 19:57:05 +0100

    Main menu implemented
    blablalblaba

---
+++ Unity/GravityRunner/Assets/Scripts/MainMenuAnimation/MenuPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MenuPlayer : MonoBehaviour
+{ 
+    Rigidbody2D rb;
+
+    MenuGravity menuGravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        menuGravity = GameObject.Find("MenuGravity").GetComponent<MenuGravity>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float gravityPlayerAngle = Vector2.SignedAngle(new Vector2(transform.up.x, transform.up.y), menuGravity.gravityUp);
+        rb.angularVelocity = 2 * gravityPlayerAngle;
+        print(Vector2.SignedAngle(menuGravity.gravityUp, new Vector2 (transform.up.x, transform.up.y)));
+    }
+}
+
