c063315b33154939fc8f80eade189229d062b181
Lukas Reutemann <70217976+Luke-537@users.noreply.github.com>
2023-01-26 23:25:18 +0100

    implemented a rough explosion where the enemy just disappears

---
+++ Unity/GravityRunner/Assets/explosionScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosionScript : MonoBehaviour
+{
+
+    public float fieldOfImpact;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.G)){
+
+            explode();
+        }
+    }
+
+    void explode(){
+
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, fieldOfImpact);
+
+        foreach(Collider2D obj in objects){
+            
+            if (obj.gameObject.tag == "Blob")
+            {
+                Destroy(obj.gameObject);
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected(){
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, fieldOfImpact);
+    }
+}
