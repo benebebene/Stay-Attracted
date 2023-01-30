2986514b51aee0fa13e73c391eb4ff32a454a468
Julez0910 <116381068+Julez0910@users.noreply.github.com>
2023-01-30 20:05:19 +0100

    Update Flash.cs

--- Unity/GravityRunner/Assets/Scripts/Flash.cs
+++ Unity/GravityRunner/Assets/Scripts/Flash.cs
@@ -1,20 +1,29 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Flash : MonoBehaviour
 {
-    private bool flashBool = true;
-    private int flashCounter = 11;
     private GameObject sprite;
+
+    private float delay = 0f;
+    private float repeatRate = 0.3f;
+
+    private bool flashBool = false;
+
+    //How many times the flash will be executed
+    private int remainingFlashes = 5;
+    
     void Start()
     {
         sprite = transform.gameObject;
+        Flashing();
     }
 
     public void Flashing()
     {
-        InvokeRepeating("FlashEffect", 0f, 0.3f);
+        InvokeRepeating("FlashEffect", delay, repeatRate);
     }
 
     void FlashEffect()
@@ -23,15 +32,14 @@ public class Flash : MonoBehaviour
         if (flashBool){
             spriteRend.color = new Color (spriteRend.color.r, spriteRend.color.g, spriteRend.color.b, 1f);
             flashBool = false;
-            flashCounter--;
         }
         else 
         {
             spriteRend.color = new Color (spriteRend.color.r, spriteRend.color.g, spriteRend.color.b, 0f);
             flashBool = true;
-            flashCounter--;
+            remainingFlashes--;
         }
-        if (flashCounter == 0)
+        if (remainingFlashes == 0)
         {
             CancelInvoke("FlashEffect");
             Destroy(sprite);
