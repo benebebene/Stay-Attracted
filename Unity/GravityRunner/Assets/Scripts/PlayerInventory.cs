e37448f4e470b941da68e19ba938912acec9f47b
Lasse Niederkrome <lasseFniederkrome@gmail.com>
2023-01-31 16:48:00 +0100

    Added DeathCounter

--- Unity/GravityRunner/Assets/Scripts/PlayerInventory.cs
+++ Unity/GravityRunner/Assets/Scripts/PlayerInventory.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     public static int numberShards { get; set; }
+    public static int deathCounter = 0;
 
     public static void ShardCollected(){
-
         numberShards++;
+    }
+
+   
+    public static void IncreaseDeathCounter(){
+        deathCounter++;
     }
 }
