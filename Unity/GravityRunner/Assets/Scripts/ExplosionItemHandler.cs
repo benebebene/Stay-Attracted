720e5b27949100d4d80b3442f6cf0dd230c8c4a9
Benedikt Marten <benedikt.marten@posteo.de>
2023-02-02 23:17:48 +0100

    new level lol

--- Unity/GravityRunner/Assets/Scripts/ExplosionItemHandler.cs
+++ Unity/GravityRunner/Assets/Scripts/ExplosionItemHandler.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Kann bei Game Over be nutzt werden
 public class ExplosionItemHandler : MonoBehaviour
 {
     void Start()
